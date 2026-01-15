9db28c7c95f6359681cbe1e5350b248b32ed8e3a
Kusu Bhavani <bhavanikusu92@gmail.com>
2026-01-15 18:15:34 +0530

    Final Submission

--- app/database.py
+++ app/database.py
@@ -3,12 +3,16 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql://postgres:password@db:5432/ledgerdb"
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    isolation_level="SERIALIZABLE"
+)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
 
-# Dependency
+
 def get_db():
     db = SessionLocal()
     try:
