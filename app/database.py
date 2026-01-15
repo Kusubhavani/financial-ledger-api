ca1494ad934a30eee161a344e9256e555038fc4b
Kusu Bhavani <bhavanikusu92@gmail.com>
2026-01-15 18:05:29 +0530

    Create database.py

---
+++ app/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql://postgres:password@db:5432/ledgerdb"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
